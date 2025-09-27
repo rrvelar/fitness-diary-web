af606311196629f5cfda0be3747f9ab52211f8ca
rrvelar <123507435+rrvelar@users.noreply.github.com>
2025-09-27 14:37:29 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app';
-import { Providers } from '../components/providers';
+import dynamic from 'next/dynamic';
+
+// ⛔ ВАЖНО: импортируем Providers динамически без SSR
+const Providers = dynamic(() => import('../components/providers').then(mod => mod.Providers), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
