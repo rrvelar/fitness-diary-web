ade209afcac7ee6fd4a35865144586f1f541c09c
rrvelar <123507435+rrvelar@users.noreply.github.com>
2025-09-29 01:15:36 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
-// pages/_app.tsx
-import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
+import "@rainbow-me/rainbowkit/styles.css"
+import type { AppProps } from "next/app"
+import { WagmiProvider } from "wagmi"
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { config } from "../wagmi"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-// ⛔ ВАЖНО: импортируем Providers динамически без SSR
-const Providers = dynamic(() => import('../components/providers').then(mod => mod.Providers), {
-  ssr: false,
-});
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
-  );
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
