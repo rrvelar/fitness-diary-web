85c1a9952a2cae26c506c209837f37c6c0daf48b
rrvelar <123507435+rrvelar@users.noreply.github.com>
2025-09-29 13:31:11 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,21 @@ import "@rainbow-me/rainbowkit/styles.css"
 import { WagmiProvider } from "wagmi"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
-import { config } from "../lib/wagmi"   // <-- правильный импорт
+import { config } from "../lib/wagmi"
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Layout from "../components/Layout"
 
 const qc = new QueryClient()
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={qc}>
         <RainbowKitProvider>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
