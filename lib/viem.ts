896fb70eb8cbd3c4e1c549ecd3e7dc13d259b23a
rrvelar <123507435+rrvelar@users.noreply.github.com>
2025-09-30 10:07:46 +0500

    Create viem.ts

---
+++ lib/viem.ts
@@ -0,0 +1,10 @@
+// lib/viem.ts
+import { createPublicClient, http } from "viem"
+import { base } from "viem/chains"
+
+export const publicClient = createPublicClient({
+  chain: base,
+  transport: http(
+    `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`
+  ),
+})
